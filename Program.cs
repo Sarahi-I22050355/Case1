6b9f0095921830c40c2b4b587ed957dee957e899
Sarah <Sarah@HP-SARAHI>
2023-08-24 19:13:43 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            int sum = SumWithForeach(numbers);
+            Console.WriteLine("The sum is: " + sum);
+        }
+
+        static int SumWithForeach(List<int> list)
+        {
+            int sum = 0;
+            foreach (int number in list)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+
+    }
+}
